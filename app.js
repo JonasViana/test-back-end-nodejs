58c7886a3ed874bb14a026a09def54417692a46f
Jonas Viana <jonasviana197@gmail.com>
2022-07-20 23:22:40 -0300

    adicionando funcao de criar usuario

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 var loginRouter = require('./routes/login')
 
 var app = express()
+app.use(bodyParser.json())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -19,6 +21,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
---
+++ controllers/UserController.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt')
+const fs = require('fs')
+const path = require('path')
+const usuariosModel = require("../database/users.json")
+
+let UserController = {
+    index:(req,res) => res.render('cadastro'),
+    store: async (req,res)=>{
+       const {usuario, email, senha, confirmarSenha} = req.body
+    
+    if(senha !== confirmarSenha){
+        return res.render('index', {erro: 'Senhas não coincidem'})
+    }
+
+    const emailExistente = usuariosModel.some(usuario => usuario.email === email)
+
+    if (emailExistente){
+        return res.render('index', {erro:'Email já utilizado , utilize outro'})
+    }
+
+    const user = {
+        usuario,
+        email,
+        senha: bcrypt.hashSync(senha, 10)
+    }
+
+    usuariosModel.push(user)
+
+    fs.writeFileSync(__dirname + '/../database/users.json', JSON.stringify(usuariosModel))
+
+    res.redirect('/login')
+}
+}
+
+module.exports = UserController
